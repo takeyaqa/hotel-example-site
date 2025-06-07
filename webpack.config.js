da0605aeb45de650b761f650bd5709b8a07e459d
Takeshi Kishi <takeyaqa@gmail.com>
2025-06-07 13:37:40 +0000

    chore: correct spelling of 'vendor' in script

--- webpack.config.js
+++ webpack.config.js
@@ -27,11 +27,11 @@ module.exports = {
         },
         {
           from: 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
-          to: 'vender/bootstrap.bundle.min.js',
+          to: 'vendor/bootstrap.bundle.min.js',
         },
         {
           from: 'node_modules/jquery/dist/jquery.min.js',
-          to: 'vender/jquery.min.js',
+          to: 'vendor/jquery.min.js',
         },
         {
           from: 'node_modules/jquery-ui/dist/themes/base/jquery-ui.min.css',
@@ -43,11 +43,11 @@ module.exports = {
         },
         {
           from: 'node_modules/jquery-ui/dist/jquery-ui.min.js',
-          to: 'vender/jquery-ui.min.js',
+          to: 'vendor/jquery-ui.min.js',
         },
         {
           from: 'node_modules/jquery-ui/ui/i18n/datepicker-ja.js',
-          to: 'vender/datepicker-ja.js',
+          to: 'vendor/datepicker-ja.js',
         },
       ],
     }),
