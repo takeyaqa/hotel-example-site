561d00f0d5a23527711d1d1a841641d4d6c87b90
Takeshi Kishi <takeyaqa@gmail.com>
2025-12-30 13:28:02 +0900

    Update Playwright configuration to use environment variable for deployed site

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ import { defineConfig, devices } from '@playwright/test';
 // import path from 'path';
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
-const useWebServer = process.env.USE_WEB_SERVER === 'true';
+const useDeployedSite = process.env.USE_DEPLOYED_SITE === 'true';
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -28,9 +28,9 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: useWebServer
-      ? 'http://localhost:8080'
-      : 'https://hotel-example-site.example.dev',
+    baseURL: useDeployedSite
+      ? 'https://hotel-example-site.example.dev'
+      : 'http://localhost:8080',
 
     /* Collect trace when retrying the failed test.See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -75,7 +75,7 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: useWebServer
+  webServer: !useDeployedSite
     ? {
         command: 'pnpm run start',
         url: 'http://localhost:8080',
