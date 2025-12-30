fca591317f98a823d843acbb0f09acc5548536d2
Takeshi Kishi <takeyaqa@gmail.com>
2025-12-30 04:55:52 +0000

    chore: update Playwright configuration for deployed site URL

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     baseURL: useDeployedSite
-      ? 'https://hotel-example-site.example.dev'
+      ? 'https://hotel-example-site.takeyaqa.dev'
       : 'http://localhost:8080',
 
     /* Collect trace when retrying the failed test.See https://playwright.dev/docs/trace-viewer */
