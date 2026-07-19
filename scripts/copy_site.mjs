72804d8fac05ec9eb6b4e184c5d34f849996211e
Takeshi Kishi <takeyaqa@icloud.com>
2026-07-19 22:06:00 +0900

    build: bundle vendor js libraries (#213)

--- scripts/copy_site.mjs
+++ scripts/copy_site.mjs
@@ -1,4 +1,4 @@
-import { copyFileSync, cpSync, existsSync, mkdirSync } from "node:fs";
+import { copyFileSync, cpSync, existsSync, mkdirSync, rmSync } from "node:fs";
 import { join } from "node:path";
 
 /**
@@ -6,14 +6,15 @@ import { join } from "node:path";
  */
 function main() {
   if (existsSync("_site")) {
-    console.error("_site directory already exsits.");
-    process.exit(1);
+    rmSync("_site", { recursive: true });
   }
   mkdirSync("_site");
+  cpSync("css", join("_site", "css"), { recursive: true });
   cpSync("data", join("_site", "data"), { recursive: true });
   cpSync("dist", join("_site", "dist"), { recursive: true });
   cpSync("en-US", join("_site", "en-US"), { recursive: true });
   cpSync("ja", join("_site", "ja"), { recursive: true });
+  cpSync("src", join("_site", "src"), { recursive: true });
   copyFileSync("index.html", join("_site", "index.html"));
 }
 
